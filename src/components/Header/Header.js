338e84b1edaff8b0772c7b7c27d9018829ddea30
ErikaStott <erika.r.stott@gmail.com>
2018-11-13 22:08:13 -0800

    more styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,15 +5,16 @@ export default function Header(){
 
     return(
         <div className= "header">
-
+            <div className="logo">
             <h1>D-low</h1>
-            <h2>Get Your Dream Life on Wheels!</h2>
-            
+            <h2>Get Your Dream Life on Wheels</h2>
+            <img alt="womandriving" src="/src/adventure-automobile-automotive-787476copy.jpg" />
+            </div>
             <div className="menu">
-            <p><Link to="/">Home</Link></p>
-            <p><Link to="/dashboard">Vehicle Profiles</Link></p>
-            <p><Link to="/newprofile">Start New Profile</Link></p>
-            <p><Link to="/">Logout</Link></p>
+            <p><Link to="/" style={{ textDecoration: 'none', color: 'white' }}>Home</Link></p>
+            <p><Link to="/dashboard" style={{ textDecoration: 'none', color: 'white' }}>Vehicle Profiles</Link></p>
+            <p><Link to="/newprofile" style={{ textDecoration: 'none', color: 'white' }}>Start New Profile</Link></p>
+            <p><Link to="/" style={{ textDecoration: 'none', color: 'white' }}>Logout</Link></p>
             </div>
         </div>
 
