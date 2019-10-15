ad851839750232c17d0dcf729612cb8108bbfe52
ErikaStott <erika.r.stott@gmail.com>
2019-10-14 19:26:03 -0700

    updated run scripts

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ const controller = require('./controller');
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '/build')));
 
 app.use(session({
     name: 'dlow',
