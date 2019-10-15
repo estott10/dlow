53a7e5f29b5bec006fb378ab7058d591e1ade5dd
ErikaStott <erika.r.stott@gmail.com>
2019-10-14 20:35:05 -0700

    updated run scripts

--- server.js
+++ server.js
@@ -22,7 +22,14 @@ const controller = require('./server/controller');
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use(session({
     name: 'dlow',
